d0ef40c83fd81b467b8198ab8f7a1a65d0c9d73d
seongjin <seongjin2427@gmail.com>
2024-02-23 15:47:48 +0900

    [feat] Navbar 컴포넌트 적용

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,53 +1,55 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from '../page/Home/Home';
-import About from '../page/About';
-import Content from '../page/Content';
-import Login from '../page/Login';
-import Info from '../page/Info';
-import Context from '../page/Context';
-import Edu from '../page/Edu';
-import SignUp from '../page/SignUp';
-import Search from '../page/Search';
-import Communication from '../page/Communication';
-import Data from '../page/Data';
-import Faq from '../page/Faq';
-import Hire from '../page/Hire';
-import Inquire from '../page/Inquire';
-import News from '../page/News';
-import Notice from '../page/Notice';
-import Program from '../page/Program';
-import Share from '../page/Share';
-import Teacher from '../page/Teacher';
-import EduDate from '../page/EduDate';
+
+import Home from '@pages/Home/Home';
+import About from '@pages/About';
+import Content from '@pages/Content';
+import Login from '@pages/Login';
+import Info from '@pages/Info';
+import Context from '@pages/Context';
+import Edu from '@pages/Edu';
+import SignUp from '@pages/SignUp';
+import Search from '@pages/Search';
+import Data from '@pages/Data';
+import Faq from '@pages/Faq';
+import Hire from '@pages/Hire';
+import Inquire from '@pages/Inquire';
+import News from '@pages/News';
+import Notice from '@pages/Notice';
+import Share from '@pages/Share';
+import Teacher from '@pages/Teacher';
+import EduDate from '@pages/EduDate';
+import Program from '@pages/Program';
+import Communication from '@pages/Communication';
+import MainLayout from '@layouts/MainLayout';
 
 const Page: React.FC = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/content" element={<Content />} />
-        <Route path="/edu" element={<Edu />} />
-        <Route path="/context" element={<Context />} />
-        <Route path="/info" element={<Info />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/communication" element={<Communication />} />
-        <Route path="/data" element={<Data />} />
-        <Route path="/eduDate" element={<EduDate />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/hire" element={<Hire />} />
-        <Route path="/inquire" element={<Inquire />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/notice" element={<Notice />} />
-        <Route path="/program" element={<Program />} />
-        <Route path="/share" element={<Share />} />
-        <Route path="/teacher" element={<Teacher />} />
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/content" element={<Content />} />
+          <Route path="/edu" element={<Edu />} />
+          <Route path="/context" element={<Context />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/communication" element={<Communication />} />
+          <Route path="/data" element={<Data />} />
+          <Route path="/eduDate" element={<EduDate />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/hire" element={<Hire />} />
+          <Route path="/inquire" element={<Inquire />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/notice" element={<Notice />} />
+          <Route path="/program" element={<Program />} />
+          <Route path="/share" element={<Share />} />
+          <Route path="/teacher" element={<Teacher />} />
+        </Routes>
+      </MainLayout>
     </BrowserRouter>
   );
 };
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 
+import Navbar from '@components/Navbar';
 import Footer from './Footer';
 
 interface MainLayoutProps {
@@ -9,6 +10,7 @@ interface MainLayoutProps {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <>
+      <Navbar />
       <main>{children}</main>
       <Footer />
     </>

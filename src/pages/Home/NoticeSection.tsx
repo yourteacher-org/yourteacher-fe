b15c45e5ca2004cc481b806ad36d5ff28c95f7b0
seongjin <seongjin2427@gmail.com>
2024-02-25 08:33:49 +0900

    [feat] 버튼 링크 추가, NoticeCard로 컴포넌트 분리

--- src/pages/Home/NoticeSection.tsx
+++ src/pages/Home/NoticeSection.tsx
@@ -3,7 +3,7 @@ import Section from '@layouts/Section';
 import Comma from '@assets/img/button_comma.svg?react';
 import TwoCircles from '@assets/icon/two-circles.svg?react';
 import Button from '@components/Button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const DATA = Array.from({ length: 4 }, (_, i) => ({
   id: String(i + 1).padStart(2, '0'),
@@ -14,7 +14,45 @@ const DATA = Array.from({ length: 4 }, (_, i) => ({
   src: '/',
 }));
 
+interface NoticeCardProps {
+  id: string;
+  title: string;
+  content: string;
+  createdDate: string;
+  src: string;
+}
+
+const NoticeCard = ({
+  id,
+  title,
+  content,
+  createdDate,
+  src,
+}: NoticeCardProps) => {
+  return (
+    <Link key={id} to={src}>
+      <li className="group w-[290px] relative">
+        <p className="mb-5 text-lg">{id}</p>
+        <p className="mb-4 text-4xl font-bold line-clamp-1">{title}</p>
+        <p className="mb-[1.875rem] text-xl leading-9 line-clamp-2 break-keep">
+          {content}
+        </p>
+        <p className="text-lg">{createdDate}</p>
+        <Comma
+          className="fill-yellow opacity-0
+            absolute -bottom-7 right-0 translate-y-28
+            group-hover:translate-y-0 group-hover:opacity-100
+            transition-all duration-[.6s] ease-out"
+        />
+      </li>
+    </Link>
+  );
+};
+
 const NoticeSection = () => {
+  const navigate = useNavigate();
+  const moveToNotice = () => navigate('/front/notify');
+
   return (
     <Section as="section" className="px-[16.25rem] py-32">
       <div className="flex items-center justify-between mb-5">
@@ -23,30 +61,15 @@ const NoticeSection = () => {
             <p className="text-xl text-green font-bold">Announcement</p>
             <TwoCircles />
           </div>
-          <h1 className="text-[60px] font-bold mb-10">공지사항</h1>
+          <h1 className="mb-10 text-[60px] font-bold">공지사항</h1>
         </div>
         <div>
-          <Button>더보기</Button>
+          <Button onClick={moveToNotice}>더보기</Button>
         </div>
       </div>
       <ul className="flex gap-[90px]">
-        {DATA.map(({ id, title, content, createdDate, src }) => (
-          <Link key={id} to={src}>
-            <li className="w-[290px] relative group">
-              <p className="text-lg mb-5">{id}</p>
-              <p className="text-4xl font-bold mb-4 line-clamp-1">{title}</p>
-              <p className="mb-[1.875rem] text-xl leading-9 line-clamp-2 break-keep">
-                {content}
-              </p>
-              <p className="text-lg">{createdDate}</p>
-              <Comma
-                className="fill-yellow opacity-0
-                  absolute -bottom-7 right-0 translate-y-28
-                  transition-all group-hover:translate-y-0 group-hover:opacity-100
-                  duration-[.6s] ease-out"
-              />
-            </li>
-          </Link>
+        {DATA.map((data) => (
+          <NoticeCard key={data.id} {...data} />
         ))}
       </ul>
     </Section>

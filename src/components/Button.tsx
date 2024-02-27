060fb6a33bb327014beec595de546a681883ed08
seongjin <seongjin2427@gmail.com>
2024-02-27 22:22:22 +0900

    [style] 모바일 반응형 스타일 적용

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,8 @@ type ButtonFontSizeType = {
 const buttonSize: ButtonFontSizeType = {
   xl: 'text-xl px-14 py-3',
   lg: `xl:min-w-72 xl:px-11 xl:py-5 xl:text-2xl
-        lg:min-w-[10.875rem] lg:py-[0.875rem] lg:text-sm`,
+        lg:min-w-[10.875rem] lg:px-[4.5rem] lg:py-[0.875rem] lg:text-sm
+        sm:min-w-[9rem] sm:px-[1.875rem] sm:text-[0.75rem]`,
   md: 'text-xl px-11 py-3',
   sm: 'text-xl px-6 py-2.5',
   xs: 'text-lg px-6 py-3',
@@ -32,9 +33,12 @@ const Button = ({
     >
       {children}
       <Comma
-        className="xl:w-[4.625rem] lg:w-[3.125rem] absolute xl:top-0 lg:-top-[1rem] -right-8 fill-green
-          transition duration-300
-          xl:group-hover:translate-x-6 lg:group-hover:translate-x-3 group-hover:drop-shadow-green"
+        className="xl:w-[4.625rem] lg:w-[3.125rem] sm:w-[2.5rem]
+          absolute xl:top-0 lg:-top-[1rem] sm:-top-[1.375rem]
+          lg:-right-8 sm:-right-6
+          fill-green transition duration-300
+          xl:group-hover:translate-x-6 lg:group-hover:translate-x-3 sm:group-hover:translate-x-2
+          group-hover:drop-shadow-green"
       />
     </button>
   );

e403096bf86e5fad75a21cbe501f7d434c6a2d85
seongjin <seongjin2427@gmail.com>
2024-03-09 18:28:08 +0900

    [chore] 자동 배포를 위한 설정 파일 수정 - vite.config.ts, resolve: ~ ./src로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   assetsInclude: ['**/*.png', '**/*.PNG'],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });

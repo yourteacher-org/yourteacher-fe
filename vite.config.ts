7760206738378ce80b8876a4e22443b5ed76a716
seongjin <seongjin2427@gmail.com>
2024-03-09 19:14:44 +0900

    [chore] 자동 배포 설정을 위한 파일 수정 (6) - vite.config.ts: ttsconfigPaths 플러그인만 유지 - 이미지 파일 확장자명 소문자로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
-import { AliasOptions, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
-import { resolve } from 'path';
-
-const aliasPaths = ['assets', 'components', 'styles', 'layouts', 'pages'];
-
-const alias: AliasOptions = Object.fromEntries(
-  aliasPaths.map((path) => [`@${path}`, resolve(__dirname, `./src/${path}`)]),
-);
-
-console.log(alias);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
-  assetsInclude: ['**/*.png', '**/*.PNG'],
-  resolve: {
-    alias,
-  },
+  assetsInclude: ['**/*.png'],
 });

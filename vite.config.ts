43e0fff7ce6ed80701be187b1526cd35f065b8b1
seongjin <seongjin2427@gmail.com>
2024-03-09 18:25:07 +0900

    [chore] 자동 배포를 위한 설정파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
   assetsInclude: ['**/*.png', '**/*.PNG'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
 });

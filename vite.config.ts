f32434a954546076a59c9df6f1f24169342fd61d
seongjin <seongjin2427@gmail.com>
2024-03-09 18:50:33 +0900

    [chore] 자동 배포를 위한 설정 파일 수정 (5) - vite.config.ts: alias @assets -> resolve ~ ./src/assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite';
+import { AliasOptions, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
-import path from 'path';
+import { resolve } from 'path';
+
+const aliasPaths = ['assets', 'components', 'styles', 'layouts', 'pages'];
+
+const alias: AliasOptions = Object.fromEntries(
+  aliasPaths.map((path) => [`@${path}`, resolve(__dirname, `./src/${path}`)]),
+);
+
+console.log(alias);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
   assetsInclude: ['**/*.png', '**/*.PNG'],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
-    },
+    alias,
   },
 });

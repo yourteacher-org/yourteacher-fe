2f9a444ba1ba91f5d845eb663ae8747ea45ea97b
이토 <59536977+seongjin2427@users.noreply.github.com>
2024-03-26 16:43:23 +0900

    [feat] 메인페이지 자료방/소통방 섹션 구현 (#33)
    * [feat] 자료 공유 섹션 UI 구현
    
    * [feat] 애니메이션 1차 구현
    
    * [feat] 애니메이션 구현 마무리
    
    * [style] 리스트 높이 변경, 메서드명 변경, 애니메이션 초기 상태 고정
    
    * [style] 태블릿, 모바일 반응형 1차 구현
    
    * [style] 데스크탑 반응형 추가 수정
    
    * [feat] 소통방 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,11 +3,13 @@ import SearchwordSection from './SearchwordSection';
 import NoticeSection from './NoticeSection';
 import RecruitSection from './RecruitSection';
 import HeroSection from './HeroSection';
+import CommunitySection from './CommunitySection';
 
 const Home: React.FC = () => {
   return (
     <div>
       <HeroSection />
+      <CommunitySection />
       <RecruitSection />
       <NoticeSection />
       <SearchwordSection />;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,50 @@ export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      screens: { sm: '390px' },
+      keyframes: {
+        'fade-in-to-left': {
+          '0%': { transform: 'translateX(7.5rem)', opacity: 0 },
+          '80%': { transform: 'translateX(0)', opacity: 1 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
+        },
+        'fade-out-to-left': {
+          '0%': { transform: 'translateX(0)', opacity: 1 },
+          '40%': { transform: 'translateX(-5rem)', opacity: 0 },
+          '100%': { transform: 'translateX(-5rem)', opacity: 0 },
+        },
+        'fade-in-to-right': {
+          '0%': { transform: 'translateX(-7.5rem)', opacity: 0 },
+          '80%': { transform: 'translateX(0)', opacity: 1 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
+        },
+        'fade-out-to-right': {
+          '0%': { transform: 'translateX(0)', opacity: 1 },
+          '40%': { transform: 'translateX(5rem)', opacity: 0 },
+          '100%': { transform: 'translateX(5rem)', opacity: 0 },
+        },
+        'move-up': {
+          '0%': { transform: 'translateY(-1px)' },
+          '20%': { transform: 'translateY(7.5px)' },
+          '80%': { transform: 'translateY(-89px)' },
+          '90%': { transform: 'translateY(-79px)' },
+          '100%': { transform: 'translateY(-84px)' },
+        },
+        'move-up-target': {
+          '0%': { transform: 'translateY(-1px)' },
+          '20%': { transform: 'translateY(7.5px)' },
+          '80%': { transform: 'translateY(-101px)' },
+          '90%': { transform: 'translateY(-91px)' },
+          '100%': { transform: 'translateY(-96px)' },
+        },
+      },
+      animation: {
+        'fade-in-to-left': 'fade-in-to-left 0.5s forwards',
+        'fade-out-to-left': 'fade-out-to-left 0.5s forwards',
+        'fade-in-to-right': 'fade-in-to-right 0.5s forwards',
+        'fade-out-to-right': 'fade-out-to-right 0.5s forwards',
+        'move-up': 'move-up 2s ease-in-out forwards',
+        'move-up-target': 'move-up-target 2s ease-in-out forwards',
+      },
       screens: { '2xl': '1921px', sm: '390px' },
       fontFamily: {
         pretendard: ['Pretendard', 'sans-serif'],

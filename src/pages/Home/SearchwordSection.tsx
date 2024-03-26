ec2edee3240ab7d2581de08e34432f6ce48ac13f
hongdayeong <dudgirl135@naver.com>
2024-03-26 16:32:36 +0900

    [fix]renderItems 로직을 불러와서 렌더링 및 변수 수정

--- src/pages/Home/SearchwordSection.tsx
+++ src/pages/Home/SearchwordSection.tsx
@@ -3,79 +3,36 @@ import { useState, useEffect } from 'react';
 import Zoom from '@assets/img/zoom.svg';
 import ChevronButton from '@components/ChevronButton';
 import { MOCK_DATA } from 'src/data/Search';
+import RenderItems from 'src/data/Render';
 
 const SearchwordSection = () => {
   const [activeItem, setActiveItem] = useState(1);
   const [isZoom, setIsZoom] = useState(true);
-  const totalItems = MOCK_DATA.length;
-  const visibleItemCount = 5;
+  const TOTAL_ITEMS = MOCK_DATA.length;
+  const VISIBLE_ITEMS_COUNT = 5;
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setActiveItem((prevItem) => (prevItem % totalItems) + 1);
+      setActiveItem((prevItem) => (prevItem % TOTAL_ITEMS) + 1);
       setIsZoom(true);
     }, 3000);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [activeItem, TOTAL_ITEMS]);
 
   const handleNextButtonClick = () => {
-    setActiveItem((prevItem) => (prevItem === totalItems ? 1 : prevItem + 1));
+    setActiveItem((prevItem) => (prevItem === TOTAL_ITEMS ? 1 : prevItem + 1));
   };
 
   const handlePrevButtonClick = () => {
-    setActiveItem((prevItem) => (prevItem === 1 ? totalItems : prevItem - 1));
+    setActiveItem((prevItem) => (prevItem === 1 ? TOTAL_ITEMS : prevItem - 1));
   };
 
   const handleZoomButonClick = () => {
     setIsZoom(true);
   };
 
-  const renderItems = () => {
-    let startIndex = activeItem - Math.floor(visibleItemCount / 2);
-    if (startIndex <= 0) {
-      startIndex += totalItems;
-    } // 현재 보여지는 아이템 목록 생성
-    return Array.from({ length: visibleItemCount }, (_, index) => {
-      let itemNumber = ((startIndex + index - 1) % totalItems) + 1;
-      if (itemNumber === 0) {
-        itemNumber = totalItems;
-      }
-      const isActive = activeItem === itemNumber;
-      return (
-        <li
-          key={itemNumber}
-          className={`mr-[12rem] relative flex items-center justify-between transition duration-300 ease-in-out xl:pl-[4rem] lg:pl-[3rem] sm:pl-[1.5rem] xl:pr-[8.25rem] lg:pr-[5rem] sm:pr-[1rem] my-8 ${
-            isActive
-              ? 'xl:text-[2rem] lg:text-[1.5rem] sm:text-[1.125rem] font-bold transform scale-125 shadow-lg rounded-full xl:w-[43.75rem] lg:w-[39.675rem] sm:w-[17.700rem] xl:h-[6rem] lg:h-[4.375rem] sm:h-[2.5rem]'
-              : 'opacity-30 xl:text-[1.5rem] lg:text-[1.25rem] sm:text-[1rem] pl-[3.5rem] pr-[2.5rem] xl:pr-[1.9rem]'
-          }`}
-        >
-          <div className="flex gap-7">
-            <span>{itemNumber}</span>
-            <p>{MOCK_DATA[itemNumber - 1].searchWord}</p>
-          </div>
-
-          <p className="xl:text-[1.5rem] lg:text-[1.375rem] sm:text-[0.875rem]">
-            조회수: {MOCK_DATA[itemNumber - 1].views}
-          </p>
-
-          {isActive && isZoom && (
-            <button
-              onClick={handleZoomButonClick}
-              className="absolute right-[1.25rem]"
-            >
-              <img
-                src={Zoom}
-                alt="링크 아이콘"
-                className="rounded-full h-20 ml-14 xl:-translate-y-1 lg:-translate-y-9 sm:-translate-y-6 xl:w-[4rem] lg:w-[3.75rem] sm:w-[1.875rem]"
-              />
-            </button>
-          )}
-        </li>
-      );
-    });
-  };
+  const items = RenderItems(activeItem, VISIBLE_ITEMS_COUNT, TOTAL_ITEMS);
 
   return (
     <Section
@@ -94,7 +51,7 @@ const SearchwordSection = () => {
           <h1 className="xl:text-[3.3rem] lg:text-[2.5rem] sm:text-[2rem] font-bold">
             인기 검색어
           </h1>
-          <div className=" gap-2 hidden xl:flex">
+          <div className="gap-2 mt-3 hidden xl:flex">
             <ChevronButton
               onClick={handlePrevButtonClick}
               direction="up"
@@ -110,7 +67,39 @@ const SearchwordSection = () => {
         </div>
       </div>
       <ul className="relative ml-[12rem]">
-        {renderItems()}
+        {/* items 배열을 매핑하여 항목을 렌더링합니다. */}
+        {items.map(({ itemNumber, isActive }) => (
+          <li
+            key={itemNumber}
+            className={`mr-[12rem] relative flex items-center justify-between transition duration-300 ease-in-out xl:pl-[4rem] lg:pl-[3rem] sm:pl-[1.5rem] xl:pr-[8.25rem] lg:pr-[5rem] sm:pr-[1rem] my-8 ${
+              isActive
+                ? 'xl:text-[2rem] lg:text-[1.5rem] sm:text-[1.125rem] font-bold transform scale-125 shadow-lg rounded-full xl:w-[43.75rem] lg:w-[39.675rem] sm:w-[17.700rem] xl:h-[6rem] lg:h-[4.375rem] sm:h-[2.5rem]'
+                : 'opacity-30 xl:text-[1.5rem] lg:text-[1.25rem] sm:text-[1rem] pl-[3.5rem] pr-[2.5rem] xl:pr-[1.9rem]'
+            }`}
+          >
+            <div className="flex gap-7">
+              <span>{itemNumber}</span>
+              <p>{MOCK_DATA[itemNumber - 1].searchWord}</p>
+            </div>
+
+            <p className="xl:text-[1.5rem] lg:text-[1.375rem] sm:text-[0.875rem]">
+              조회수: {MOCK_DATA[itemNumber - 1].views}
+            </p>
+
+            {isActive && isZoom && (
+              <button
+                onClick={handleZoomButonClick}
+                className="absolute right-[1.25rem]"
+              >
+                <img
+                  src={Zoom}
+                  alt="링크 아이콘"
+                  className="rounded-full h-20 ml-14 xl:-translate-y-1 lg:-translate-y-9 sm:-translate-y-6 xl:w-[4rem] lg:w-[3.75rem] sm:w-[1.875rem]"
+                />
+              </button>
+            )}
+          </li>
+        ))}
 
         <div
           className="

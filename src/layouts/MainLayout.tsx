6ac9f65bc86fea9540bc165ae5ff49f5959ebd5c
seongjin <seongjin2427@gmail.com>
2024-03-26 21:14:13 +0900

    [fix] 인기검색어 섹션, totalItems -> TOTAL_ITEMS로 수정

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,8 @@
 import { ReactNode } from 'react';
 
 import Navbar from '@components/Navbar';
-import SearchwordSection from '@pages/Home/SearchwordSection';
+import Footer from './Footer';
 
-// import Footer from './Footer';
 interface MainLayoutProps {
   children?: ReactNode;
 }
@@ -11,11 +10,11 @@ interface MainLayoutProps {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <>
-
-      <Navbar />    
+      <Navbar />
       <main className="xl:max-w-[1920px] lg:max-w-[1024px] sm:max-w-[390px] mx-auto">
         {children}
       </main>
+      <Footer />
     </>
   );
 };

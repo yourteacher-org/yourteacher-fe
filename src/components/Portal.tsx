f11690b25f9db4b09156bc31e0cf4af33de3e3f4
seongjin <seongjin2427@gmail.com>
2024-05-14 16:39:08 +0900

    [feat] 모달창 구현을 위한 Portal 컴포넌트 구현

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -2,15 +2,11 @@ import { ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 
 interface PortalProps {
-  children?: ReactNode;
+  children: ReactNode;
 }
 
 const Portal = ({ children }: PortalProps) => {
-  const container = document.getElementById('root');
-
-  if (!container) {
-    throw new Error('container 노드가 존재하지 않습니다.');
-  }
+  const container = document.getElementById('modal')!;
 
   return createPortal(children, container);
 };

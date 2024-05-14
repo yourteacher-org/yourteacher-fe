c8c09fddb42473302872cab039b27f0888cee021
seongjin <seongjin2427@gmail.com>
2024-05-14 16:33:09 +0900

    [style] UserAction last-of-type 선택자 적용 - 마지막 타입의 요소만 border-bottom 삭제

--- src/pages/User/UserAction.tsx
+++ src/pages/User/UserAction.tsx
@@ -1,17 +1,17 @@
 interface UserActionProps {
   title: string;
   count: number | string;
-  borderBottom?: boolean;
 }
 
-const UserAction = ({ title, count, borderBottom }: UserActionProps) => {
+const UserAction = ({ title, count }: UserActionProps) => {
   return (
     <div
-      className={`flex items-center justify-between
+      className="flex items-center justify-between
         2xl:py-[1.875rem] xl:py-[1.5vw] lg:py-4 sm:py-2
         xl:pl-10 lg:pl-8 sm:pl-4
         xl:pr-20 lg:pr-10 sm:pr-8
-      ${borderBottom && 'border-b-2 border-gray-5'}`}
+        border-b-2 border-gray-5
+        last-of-type:border-none"
     >
       <span className="2xl:text-[1.625rem] xl:text-[1.375vw] lg:text-lg sm:text-[0.875rem]">
         {title}

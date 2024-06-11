ae9fd53735914dfbea0fc3cfffa96b468d60da30
seongjin <seongjin2427@gmail.com>
2024-06-11 16:37:21 +0900

    [refact] UserNickNameModal 컴포넌트 input 렌더링 최적화

--- src/pages/User/UserModalContent/UserNickNameModal.tsx
+++ src/pages/User/UserModalContent/UserNickNameModal.tsx
@@ -1,8 +1,40 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, Ref, forwardRef, useRef, useState } from 'react';
 
 import { Modal, useModalContext } from '@components/Modal';
 import { UserModalActionType } from '@/types/user';
 import SVGIcon from '@components/SVGIcon';
+
+interface UserNicknameInputButtonProps {
+  children?: (nickname: string) => void;
+  resetDuplicateStatus: () => void;
+}
+
+const UserNicknameInputButton = forwardRef(
+  (
+    { children, resetDuplicateStatus }: UserNicknameInputButtonProps,
+    ref: Ref<HTMLInputElement>,
+  ) => {
+    const [nickname, setNickname] = useState<string>('');
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setNickname(e.target.value);
+      resetDuplicateStatus();
+    };
+
+    return (
+      <>
+        <input
+          ref={ref}
+          value={nickname}
+          onChange={handleChange}
+          placeholder="기존 설정한 닉네임: 유어티처"
+          className="flex-1 min-w-[14.75rem] focus:outline-none"
+        />
+        {children && children(nickname)}
+      </>
+    );
+  },
+);
 
 const DUPLICATE_STATUS = {
   DEFAULT: 'DEFAULT',
@@ -37,30 +69,33 @@ interface UserNickNameModalProps {
 
 const UserNickNameModal = ({ children }: UserNickNameModalProps) => {
   const { handleIsShow } = useModalContext();
-  const [nickname, setNickname] = useState<string>('');
   const [duplicateStatus, setDuplicateStatus] = useState<
     keyof typeof DUPLICATE_STATUS
   >(DUPLICATE_STATUS.DEFAULT);
+  const nickNameRef = useRef<HTMLInputElement>(null);
 
   const confirm = () => {
-    console.log(nickname);
+    console.log(nickNameRef.current?.value);
     handleIsShow(false);
   };
 
   const cancel = () => handleIsShow(false);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setNickname(e.target.value);
+  const resetDuplicateStatus = () =>
     setDuplicateStatus(DUPLICATE_STATUS.DEFAULT);
-  };
 
-  const checkDuplicate = async () => {
+  const checkDuplicate = async (nickname: string) => {
     if (nickname === 'nickname') {
       setDuplicateStatus(DUPLICATE_STATUS.DUPLICATE);
     } else {
       setDuplicateStatus(DUPLICATE_STATUS.NOT_DUPLICATE);
     }
   };
+
+  const confirmNotDuplicated =
+    duplicateStatus === DUPLICATE_STATUS.NOT_DUPLICATE;
+
+  const isDuplicateNickName = duplicateStatus === DUPLICATE_STATUS.DUPLICATE;
 
   return (
     <Modal>
@@ -71,26 +106,26 @@ const UserNickNameModal = ({ children }: UserNickNameModalProps) => {
           className={`flex border rounded-full p-1 pl-5 mb-1 overflow-hiddenm
             ${DUPLICATE_STATUS_OPTIONS[duplicateStatus].BORDER_COLOR}`}
         >
-          <input
-            className="flex-1 min-w-[14.75rem] focus:outline-none"
-            placeholder="기존 설정한 닉네임: 유어티처"
-            onChange={handleChange}
-            value={nickname}
-          />
-          <button
-            className={`px-3 py-1 ml-2
-              bg-gray-1 disabled:bg-gray-5 hover:bg-green
-              text-white
-              ${duplicateStatus === 'NOT_DUPLICATE' && 'bg-green'}
-              transition-colors rounded-full`}
-            disabled={!nickname.length}
-            onClick={checkDuplicate}
+          <UserNicknameInputButton
+            ref={nickNameRef}
+            resetDuplicateStatus={resetDuplicateStatus}
           >
-            {DUPLICATE_STATUS_OPTIONS[duplicateStatus].BUTTON}
-          </button>
+            {(nickname) => (
+              <button
+                className={`px-3 py-1 ml-2
+                bg-gray-1 disabled:bg-gray-5 hover:bg-green
+                text-white transition-colors rounded-full
+                ${confirmNotDuplicated && 'bg-green'}`}
+                disabled={!nickname.length || isDuplicateNickName}
+                onClick={() => checkDuplicate(nickname)}
+              >
+                {DUPLICATE_STATUS_OPTIONS[duplicateStatus].BUTTON}
+              </button>
+            )}
+          </UserNicknameInputButton>
         </div>
         <div className="flex gap-1.5 items-center">
-          {duplicateStatus === DUPLICATE_STATUS.DUPLICATE && (
+          {isDuplicateNickName && (
             <i className="flex items-center justify-center w-4 h-4 rounded-full bg-red">
               <SVGIcon type="Exclamation" width="10" height="10" />
             </i>

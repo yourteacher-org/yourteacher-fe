7966b5cfd890263463c028e1baab569d85a132fd
seongjin <seongjin2427@gmail.com>
2024-07-28 13:40:44 +0900

    [style] 태블릿 반응형 적용

--- src/pages/Withdraw/Withdraw.tsx
+++ src/pages/Withdraw/Withdraw.tsx
@@ -10,10 +10,10 @@ const Withdraw: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="h-[90vh] flex justify-center pt-10">
+    <div className="xl:h-[90vh] lg:h-[60vh] flex justify-center pt-10">
       <div className="flex flex-col items-center">
-        <img src={Logo} alt="logo" className="mb-20" />
-        <div className="flex gap-2 mb-24">
+        <img src={Logo} alt="logo" className="lg:w-40 xl:mb-20 lg:mb-14" />
+        <div className="hidden xl:flex gap-2 mb-24">
           {dots.map((dot) => (
             <span
               key={dot}
@@ -21,9 +21,11 @@ const Withdraw: React.FC = () => {
             />
           ))}
         </div>
-        <div className="flex flex-col items-center mb-20">
-          <h1 className="text-7xl font-bold mb-10">회원탈퇴 완료</h1>
-          <p className="text-3xl text-center leading-10">
+        <div className="flex flex-col items-center xl:mb-20 lg:mb-10">
+          <h1 className="xl:text-7xl lg:text-4xl font-bold xl:mb-10 lg:mb-5">
+            회원탈퇴 완료
+          </h1>
+          <p className="xl:text-3xl lg:text-2xl text-center leading-10">
             탈퇴 후, 7일 동안 재가입이 불가합니다. <br />
             유어티처를 이용해주시고 사랑해주셔서 감사합니다.
           </p>

87dcbe2977bd86dd955ebf2dacae04d196610144
seongjin <seongjin2427@gmail.com>
2024-08-27 16:28:28 +0900

    [test] useOutletContext 커스텀 훅 사용 테스트

--- src/pages/Share/Collection.tsx
+++ src/pages/Share/Collection.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useOutletContext } from 'react-router-dom';
+
+import { CategoryType } from '@/types/board';
 
 const Collection: React.FC = () => {
+  const { category, subclass } = useOutletContext<CategoryType>();
+
+  console.log(category, subclass);
   return (
     <div>
       <h2>Collection</h2>

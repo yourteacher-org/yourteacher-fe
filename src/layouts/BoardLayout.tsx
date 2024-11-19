6fa4983458c252099de6faccd7b3369116242591
이토 <59536977+seongjin2427@users.noreply.github.com>
2024-11-19 17:08:46 +0900

    [feat] 일반 게시판 공통 요소 재사용 UI 구현 (#51)
    * [fix] 불필요한 폴더 삭제
    
    * [fix] HeroSection 이미지 원인으로 스크롤 바 생성되는 버그 수정
    
    * [fix] ChevronButton 원 형태 나오도록 수정
    
    * [style] 섹션 간 간격 수정
    
    * [fix] 각종 버그 수정
    
    * [feat] 태블릿 이해 네비바, url 수정
    
    * [feat] SNS 로그인 별 이름 추가
    
    * [feat] 공통 게시판 제목 및 depth navigation 스타일 추가, 배경 추가
    
    * [style] 공지사항 섹션 가운데 정렬 적용

--- src/layouts/BoardLayout.tsx
+++ src/layouts/BoardLayout.tsx
@@ -1,15 +1,17 @@
 import { ReactNode } from 'react';
 
+import BoardComma from '@assets/icon/board_comma.svg?react';
+
 interface BoardLayoutProps {
   children?: ReactNode;
 }
 
 const BoardLayout = ({ children }: BoardLayoutProps) => {
   return (
-    <div className="">
-      <h1>BoardLayout</h1>
-      {children}
-    </div>
+    <>
+      <BoardComma className="absolute top-0 left-9 -z-10" />
+      <div className="xl:px-[16.25rem] xl:pt-40 xl:pb-72">{children}</div>
+    </>
   );
 };
 

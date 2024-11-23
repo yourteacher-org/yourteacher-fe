8ac32e5508ee0ceca481fa2ca5daccc0b4194a9b
seongjin <seongjin2427@gmail.com>
2024-11-23 17:22:02 +0900

    [feat] 하트, 북마크 아이콘 추가, 반응형 태블릿 추가

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -2,6 +2,9 @@ import { useParams } from 'react-router-dom';
 
 import ClipIcon from '@assets/icon/clip.svg?react';
 import DownloadIcon from '@assets/icon/download.svg?react';
+import BookmarkIcon from '@assets/icon/bookmark';
+import HeartIcon from '@assets/icon/heart';
+import PageMover from '@components/PageMover';
 
 const MOCK_HASHTAGS = [
   '유어티처',
@@ -18,15 +21,18 @@ const Board = () => {
   console.log(category, subclass, postId);
 
   return (
-    <div>
-      <div className="xl:w-[87.5rem] xl:min-h-[81.25rem] border-y-2 border-black">
-        <div className="py-10 mb-[3.875rem] border-b-2">
-          <p className="mb-8 text-center text-4xl font-bold">
+    <div className="mb-32">
+      <div className="xl:w-[87.5rem] xl:min-h-[81.25rem] lg:mb-[7.5rem] border-y-2 border-black">
+        <div className="relative xl:py-10 lg:py-7 xl:mb-[3.875rem] lg:mb-[1.875rem] border-b-2">
+          <BookmarkIcon className="absolute right-5 xl:top-6 lg:top-7" />
+          <p className="xl:mb-8 lg:mb-6 text-center xl:text-4xl lg:text-[1.625rem] font-bold">
             정보나눔게시판은 정보를 공유하는 게시판입니다.(TEST)
           </p>
-          <div className="w-full flex justify-center gap-[3.125rem] text-xl">
+          <div className="w-full flex justify-center xl:gap-[3.125rem] lg:gap-5 xl:text-xl lg:text-[1rem]">
             <span>글쓴이 : 김티처</span>
+            <div className="w-[1px] h-auto bg-gray-5" />
             <span>등록일 : 2024-00-00</span>
+            <div className="w-[1px] h-auto bg-gray-5" />
             <span>조회소 : 1,582</span>
           </div>
         </div>
@@ -41,17 +47,21 @@ const Board = () => {
               전국각지의 어린이집 · 유치원 선생님들과 다양한 정보를 나눠보세요. (TEST)`}
           </p>
         </div>
-        <ul className="flex gap-4 mb-[2.5rem]">
+        <ul className="relative flex gap-4 xl:mb-[2.5rem] lg:mb-[1.875rem]">
           {MOCK_HASHTAGS.map((tag) => (
             <li
               key={tag}
-              className="px-[1.5625rem] py-[0.625rem] rounded-full border-2 border-gray-1 text-xl text-gray-1 leading-9"
+              className="xl:px-[1.5625rem] lg:px-6 xl:py-[0.625rem] lg:py-3 rounded-full border-2 border-gray-1 text-xl text-gray-1 leading-9"
             >
               # {tag}
             </li>
           ))}
+          <div className="absolute bottom-3 right-0 flex flex-col gap-2 items-center">
+            <HeartIcon />
+            <p>1258</p>
+          </div>
         </ul>
-        <div className="flex border-t-2 text-xl py-[2.5rem]">
+        <div className="flex border-t-2 text-xl xl:py-[2.5rem] lg:py-7">
           <div className="flex justify-center gap-[2px] mr-[2.5rem] pl-[1.25rem] font-medium">
             <i>
               <ClipIcon />
@@ -73,6 +83,7 @@ const Board = () => {
           </ul>
         </div>
       </div>
+      <PageMover />
     </div>
   );
 };
